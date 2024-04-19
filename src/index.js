cb7d51c062dd26c46a4cf8e487c327f441e817a0
nicknamed <nicknamed19@gmail.com>
2024-04-19 16:33:21 -0500

    Se crea la funcion para crear nuevos todos y la funcion para completar una tarea de forma visual y dentro de la data

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import "./css/base.css";
 import { mainInput } from "./js/nodes.js";
-import { newTodo, newObjTodo } from "./js/utils.js";
+import { newTodo } from "./js/utils.js";
 
 let textValue;
 
 mainInput.addEventListener("keydown", (event) => {
   if (event.key === "Enter") {
-    newObjTodo(textValue);
     newTodo(textValue);
   }
 });
--- src/js/utils.js
+++ src/js/utils.js
@@ -1,17 +1,19 @@
-import { todoList } from "./nodes.js";
+import { todoList, mainInput } from "./nodes.js";
 const data = [];
 
-function newObjTodo(text) {
+function generateUniqueId() {
+  return Date.now().toString(36) + Math.random().toString(36).substr(2);
+}
+
+function newTodo(text) {
   let newObj = {
     title: text,
     completed: false,
     edit: false,
+    id: generateUniqueId(),
   };
   data.push(newObj);
-  console.log(data);
-}
 
-function newTodo(text) {
   const li = document.createElement("li");
 
   const div = document.createElement("div");
@@ -20,6 +22,7 @@ function newTodo(text) {
   const input = document.createElement("input");
   input.classList.add("toggle");
   input.type = "checkbox";
+  input.addEventListener("click", () => onCompleted(li, newObj.id));
 
   const lable = document.createElement("label");
   lable.textContent = text;
@@ -30,6 +33,17 @@ function newTodo(text) {
   div.append(input, lable, button);
   li.appendChild(div);
   todoList.appendChild(li);
+
+  mainInput.value = "";
 }
 
-export { newTodo, newObjTodo };
+function onCompleted(li, id) {
+  li.classList.toggle("completed");
+
+  const filterArray = data.filter((obj) => obj.id === id);
+  filterArray.forEach((obj) =>
+    obj.completed ? (obj.completed = false) : (obj.completed = true)
+  );
+}
+
+export { newTodo };
